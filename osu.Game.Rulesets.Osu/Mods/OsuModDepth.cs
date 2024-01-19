eb2a81ecb7aa437c9771262bec98aaa159625fae
PuddlePumpkin <66428305+PuddlePumpkin@users.noreply.github.com>
2024-01-19 00:00:56 -0600

    shtuff

--- osu.Game.Rulesets.Osu/Mods/OsuModDepth.cs
+++ osu.Game.Rulesets.Osu/Mods/OsuModDepth.cs
@@ -5,8 +5,10 @@ using System;
 using System.Linq;
 using osu.Framework.Bindables;
 using osu.Framework.Extensions.ObjectExtensions;
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Localisation;
+using osu.Game.Beatmaps;
 using osu.Game.Configuration;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Objects.Drawables;
@@ -18,7 +20,7 @@ using osuTK;
 
 namespace osu.Game.Rulesets.Osu.Mods
 {
-    public class OsuModDepth : ModWithVisibilityAdjustment, IUpdatableByPlayfield, IApplicableToDrawableRuleset<OsuHitObject>
+    public class OsuModDepth : ModWithVisibilityAdjustment, IUpdatableByPlayfield, IApplicableToDrawableRuleset<OsuHitObject>, IApplicableToDifficulty
     {
 
         public override string Name => "Depth";
@@ -32,24 +34,33 @@ namespace osu.Game.Rulesets.Osu.Mods
         private static readonly Vector3 camera_position = new Vector3(OsuPlayfield.BASE_SIZE.X * 0.5f, OsuPlayfield.BASE_SIZE.Y * 0.5f, -200);
         private readonly float sliderMinDepth = depthForScale(1.5f); // Depth at which slider's scale will be 1.5f
 
-        [SettingSource("Maximum depth", "How far away objects appear.", 0)]
-        public BindableFloat MaxDepth { get; } = new BindableFloat(100)
+        [SettingSource("Scroll Speed", "How fast to scroll.", 0)]
+        public BindableFloat ScrollSpeed { get; } = new BindableFloat(5)
         {
-            Precision = 10,
-            MinValue = 50,
-            MaxValue = 3000
+            Precision = 0.5f,
+            MinValue = 1,
+            MaxValue = 10
         };
 
-        [SettingSource("Show Approach Circles", "Whether approach circles should be visible.", 1)]
-        public BindableBool ShowApproachCircles { get; } = new BindableBool(true);
-
-        [SettingSource("Para Amnt", "How far away objects appear.", 0)]
+        [SettingSource("Parralax Amount", "Ratio of cursor to circle motion.", 0)]
         public BindableFloat ParaAmount { get; } = new BindableFloat(0.25f)
         {
             Precision = 0.05f,
             MinValue = 0,
             MaxValue = 1
         };
+
+        [SettingSource("Circle Opacity", "How opaque should objects be.", 0)]
+        public BindableFloat CircleOpacity { get; } = new BindableFloat(85)
+        {
+            Precision = 5,
+            MinValue = 10,
+            MaxValue = 100
+        };
+
+        [SettingSource("Show Judgements", "Whether judgements should be visible.", 1)]
+        public BindableBool ShowJudgements { get; } = new BindableBool(false);
+
         protected override void ApplyIncreasedVisibilityState(DrawableHitObject hitObject, ArmedState state) => applyTransform(hitObject, state);
 
         protected override void ApplyNormalVisibilityState(DrawableHitObject hitObject, ArmedState state) => applyTransform(hitObject, state);
@@ -58,16 +69,17 @@ namespace osu.Game.Rulesets.Osu.Mods
         {
             // Hide judgment displays and follow points as they won't make any sense.
             // Judgements can potentially be turned on in a future where they display at a position relative to their drawable counterpart.
-            drawableRuleset.Playfield.DisplayJudgements.Value = false;
+            drawableRuleset.Playfield.DisplayJudgements.Value = ShowJudgements.Value;
             (drawableRuleset.Playfield as OsuPlayfield)?.FollowPoints.Hide();
         }
 
         private void applyTransform(DrawableHitObject drawable, ArmedState state)
         {
+            drawable.Alpha = MapRange(CircleOpacity.Value, 10, 100, 0.1f, 1f);
             switch (drawable)
             {
                 case DrawableHitCircle circle:
-                    if (!ShowApproachCircles.Value)
+                    if (!false)
                     {
                         var hitObject = (OsuHitObject)drawable.HitObject;
                         double appearTime = hitObject.StartTime - hitObject.TimePreempt;
@@ -103,24 +115,37 @@ namespace osu.Game.Rulesets.Osu.Mods
         {
             var hitObject = drawable.HitObject;
             // Circles are always moving at the constant speed. They'll fade out before reaching the camera even at extreme conditions (AR 11, max depth).
-            double speed = MaxDepth.Value / hitObject.TimePreempt;
+            double speed = 2000 / hitObject.TimePreempt;
             double appearTime = hitObject.StartTime - hitObject.TimePreempt;
-            float z = MaxDepth.Value - (float)((Math.Max(time, appearTime) - appearTime) * speed);
+            float z = 2000 - (float)((Math.Max(time, appearTime) - appearTime) * speed);
 
             float scale = scaleForDepth(z);
             drawable.Position = toPlayfieldPosition(scale, hitObject.StackedPosition, cursorPos);
             drawable.Scale = new Vector2(scale);
         }
+        public static float MapRange(float from, float fromMin, float fromMax, float toMin, float toMax)
+        {
+            float fromAbs = from - fromMin;
+            float fromMaxAbs = fromMax - fromMin;
 
+            float normal = fromAbs / fromMaxAbs;
+
+            float toMaxAbs = toMax - toMin;
+            float toAbs = toMaxAbs * normal;
+
+            float to = toAbs + toMin;
+
+            return to;
+        }
         private void processSlider(double time, DrawableSlider drawableSlider, Vector2 cursorPos)
         {
             var hitObject = drawableSlider.HitObject;
 
-            double baseSpeed = MaxDepth.Value / hitObject.TimePreempt;
+            double baseSpeed = 2000 / hitObject.TimePreempt;
             double appearTime = hitObject.StartTime - hitObject.TimePreempt;
 
             // Allow slider to move at a constant speed if its scale at the end time will be lower than 1.5f
-            float zEnd = MaxDepth.Value - (float)((Math.Max(hitObject.StartTime + hitObject.Duration, appearTime) - appearTime) * baseSpeed);
+            float zEnd = 2000 - (float)((Math.Max(hitObject.StartTime + hitObject.Duration, appearTime) - appearTime) * baseSpeed);
 
             if (zEnd > sliderMinDepth)
             {
@@ -166,6 +191,18 @@ namespace osu.Game.Rulesets.Osu.Mods
         {
             Vector2 cursorrelative = new Vector2(cursorPos.X - 1920f / 8f, cursorPos.Y - 1080f / 8f);
             return (positionAtZeroDepth - camera_position.Xy) * scale + camera_position.Xy + ((cursorrelative * -ParaAmount.Value) * (new Vector2(scale, scale) / 1f));
+        }
+
+        public void ApplyToDifficulty(BeatmapDifficulty difficulty) => ApplySettings(difficulty);
+
+        /// <summary>
+        /// Apply all custom settings to the provided beatmap.
+        /// </summary>
+        /// <param name="difficulty">The beatmap to have settings applied.</param>
+        protected virtual void ApplySettings(BeatmapDifficulty difficulty)
+        {
+            float ApproachRate = MapRange(ScrollSpeed.Value, 1, 10, -10, 10);
+            difficulty.ApproachRate = ApproachRate;
         }
     }
 }
